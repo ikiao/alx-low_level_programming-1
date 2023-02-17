fa63dc1d8996f9dc91bfa68fe102da2021324b69
Ernest <ekiromo50@gmail.com>
2023-02-17 09:25:44 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - printing numbers od base 10
+ *for - loop to aid in printing
+ *Return: zero
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
