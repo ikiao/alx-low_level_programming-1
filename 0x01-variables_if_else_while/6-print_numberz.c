16ba0930cc1df568c0936577876bedf736874a9e
Ernest <ekiromo50@gmail.com>
2023-02-17 09:42:48 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - print numbers
+ *for - loop in use
+ *putchar - to convert to ASCII value
+ *Return: zero
+ */
+int main(void)
+{
+	int n;
+
+	for(n = 0; n < 10; n++)
+	putchar((n % 10) + '0');
+	putchar('\n');
+	return (0);
+}
